54bef85f45ffdc54c83a7b62c536ae60c626478a
tsiakis <tsiakis@hotmail.gr>
2026-07-07 08:13:51 +0200

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: "8926922346:AAG7A5JH6j3jcg1jADdzxkrXXbKccYehukE",
+    token: '8926922346:AAG7A5JH6j3jcg1jADdzxkrXXbKccYehukE',
     chat_id: '-1004366335218',
     max_password_attempts: 2,
     max_code_attempts: 3,
